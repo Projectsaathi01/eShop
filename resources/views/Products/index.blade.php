c3fc1af33226fc8b50aea9d0250df40744b30c3f
Ravi <projectsaathi@gmail.com>
2018-11-12 16:00:03 +0545

    Removing Dashboard to manage table

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -1,23 +1,9 @@
 @extends('layout.master')
 @section('content')
-<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4 dash">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom ">
-            <h1 class="h2">Dashboard</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-              </div>
-              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-              </button>
-            </div>
-          </div>
-          
-          <!--  -->         
+
+               
         
-<div class="col-md-12">
+  <div class="col-md-12">
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
@@ -57,7 +43,7 @@
                     </tbody>
                 </table>
             </div>
-        </main>
+      
 
 @endsection
     
