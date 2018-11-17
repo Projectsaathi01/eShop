9903a49827bec846d26502500c637c0a24f937a4
NamRoshan <deubaroshan66@gmail.com>
2018-11-17 14:16:59 +0545

    bottsrap error solve

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,5 +76,7 @@
             @yield('content')
         </main>
     </div>
+    <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
 </body>
 </html>
